d91e6f56107649dd26ff24fa80e9c9c316c8a411
shudipto <shudipta15-11424@diu.edu.bd>
2021-10-30 20:55:14 +0600

    authprovider update

--- src/App.js
+++ src/App.js
@@ -11,40 +11,43 @@ import NotFound from './components/NotFound/NotFound';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Footer from './components/shared/Footer/Footer';
 import Header from './components/shared/Header/Header';
+import AuthProvider from './context/AuthProvider';
 
 function App() {
   return (
     <div className="w-full">
-      <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/" >
-            <Home></Home>
-          </Route>
-          <Route path="/home" >
-            <Home></Home>
-          </Route>
-          <PrivateRoute path="/booking/:packageId" >
-            <Booking></Booking>
-          </PrivateRoute>
-          <PrivateRoute path="/myBookings" >
-            <MyBookings></MyBookings>
-          </PrivateRoute>
-          <PrivateRoute path="/allBookings" >
-            <AllBookings></AllBookings>
-          </PrivateRoute>
-          <PrivateRoute path="/add-package" >
-            <AddPackage></AddPackage>
-          </PrivateRoute>
-          <Route path="/login" >
-            <Login></Login>
-          </Route>
-          <Route path="*" >
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-        <Footer></Footer>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/" >
+              <Home></Home>
+            </Route>
+            <Route path="/home" >
+              <Home></Home>
+            </Route>
+            <PrivateRoute path="/booking/:packageId" >
+              <Booking></Booking>
+            </PrivateRoute>
+            <PrivateRoute path="/myBookings" >
+              <MyBookings></MyBookings>
+            </PrivateRoute>
+            <PrivateRoute path="/allBookings" >
+              <AllBookings></AllBookings>
+            </PrivateRoute>
+            <PrivateRoute path="/add-package" >
+              <AddPackage></AddPackage>
+            </PrivateRoute>
+            <Route path="/login" >
+              <Login></Login>
+            </Route>
+            <Route path="*" >
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
